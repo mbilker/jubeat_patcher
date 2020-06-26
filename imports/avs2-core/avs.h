c9a069dc0ab15bc1b8e3c6befec7d44c1d7f29a4
Matt Bilker <me@mbilker.us>
2020-06-26 23:11:08 +0000

    imports(avs2-core): add property_mem_read mapping

--- imports/avs2-core/avs.h
+++ imports/avs2-core/avs.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,6 +15,13 @@ void __declspec(dllimport) log_body_fatal(const char *module, const char *fmt, .
 
 size_t __declspec(dllimport) avs_strlcpy(char *dst, const char *src, size_t dst_size);
 size_t __declspec(dllimport) avs_snprintf(char *dst, size_t dst_size, const char *fmt, ...);
+
+void *__declspec(dllimport) property_mem_read(
+        void *prop,
+        void *prop_node,
+        unsigned int flags,
+        uint8_t *buffer,
+        uint32_t buffer_size);
 
 #ifdef __cplusplus
 };
